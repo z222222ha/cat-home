c6d604d9b1152c1437760f21abef10263780b907
ZHH <zhang2ha@qq.com>
2025-04-29 09:15:28 +0800

    feat：pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "cat-home-page",
-      script: "npm",
-      args: "run start",
-      autorestart: true,
-      watch: true,
+      name: "cat-home",
+      script: "pnpm",
+      args: "start",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
+        PORT: 3000, // 指定端口
       },
     },
   ],
